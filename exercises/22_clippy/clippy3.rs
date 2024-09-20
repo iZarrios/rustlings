f81b411073a21e745b6a814d3983e02de5dcd95d
iZarrios <zarriose@gmail.com>
2024-09-20 09:15:17 +0300

    finished clippy

---
+++ exercises/22_clippy/clippy3.rs
@@ -0,0 +1,28 @@
+// Here are some more easy Clippy fixes so you can see its utility 📎
+// TODO: Fix all the Clippy lints.
+
+#[rustfmt::skip]
+#[allow(unused_variables, unused_assignments)]
+fn main() {
+    let my_option: Option<()> = None;
+    if my_option.is_some() {
+        println!("{:?}", my_option);
+    }
+
+    let my_arr = &[
+        -1, -2, -3,
+        -4, -5, -6,
+    ];
+    println!("My array! Here it is: {my_arr:?}");
+
+    // let my_empty_vec = vec![1, 2, 3, 4, 5].resize(0, 5);
+    println!("This Vec is empty, see? {:?}",());
+
+    let value_a = 45;
+    let value_b = 66;
+    // Let's swap these two!
+    // value_a = value_b;
+    // value_b = value_a;
+    std::mem::swap(&mut &value_a, &mut &value_b);
+    println!("value a: {value_a}; value b: {value_b}");
+}
